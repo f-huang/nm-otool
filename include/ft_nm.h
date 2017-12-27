dea16846a8cda16ecee1c007392423273f97acd7
Fanny HUANG <fhuang@e3r4p18.42.fr>
2017-12-27 09:48:22 +0100

    Add include

---
+++ include/ft_nm.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_nm.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fhuang <fhuang@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/27 08:40:36 by fhuang            #+#    #+#             */
+/*   Updated: 2017/12/27 08:50:18 by fhuang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_NM_H
+# define FT_NM_H
+
+# define DEFAULT_FILE "a.out"
+
+/*
+**	OPTIONS
+**
+**	 -n     Sort numerically rather than alphabetically.
+**	 -r     Sort in reverse order.
+**	 -u     Display only undefined symbols.
+**	 -U     Don't display undefined symbols.
+**	 -x     Display the symbol table entry's fields in hexadecimal, along with the name as a string.
+**	 -j     Just display the symbol names (no value or type).
+**	 -t     format
+**	            d      The value shall be written in decimal (default).
+**	            o      The value shall be written in octal.
+**	            x      The value shall be written in hexadecimal.
+**
+*/
+#endif
---
+++ include/ft_otool.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_otool.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fhuang <fhuang@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/27 08:40:36 by fhuang            #+#    #+#             */
+/*   Updated: 2017/12/27 09:47:41 by fhuang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_OTOOL_H
+# define FT_OTOOL_H
+
+/*
+**	OPTIONS
+**
+**	-t     Display the contents of the (__TEXT,__text) section.
+**	       With the -v flag, this disassembles the text.
+**	       And with -V, it also symbolically disassembles the operands.
+**	-d     Display the contents of the (__DATA,__data) section.
+**	-o     Display the contents of the __OBJC segment used by the Objective-C run-time system.
+**	-h     Display the Mach header.
+**	-l     Display the load commands.
+**	-v     Display verbosely (symbolically) when possible.
+**	-V     Display  the disassembled operands symbolically (this implies the -v option).
+*/
+
+#endif
