08717c281ffbf13d63ccc640897b9e22dee60e3a
Fanny HUANG <fhuang@e3r4p22.42.fr>
2017-12-28 19:15:55 +0100

    WIP

--- src/nm/get_symbols.c
+++ src/nm/get_symbols.c
@@ -6,32 +6,88 @@
 /*   By: fhuang <fhuang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/27 13:02:35 by fhuang            #+#    #+#             */
-/*   Updated: 2017/12/27 14:25:48 by fhuang           ###   ########.fr       */
+/*   Updated: 2017/12/28 19:09:33 by fhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <mach-o/loader.h>
+#include <mach-o/nlist.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include "libft.h"
 #include "ft_nm.h"
 
-static int	name_list(t_nm *nm, void *ptr)
+static void	handle_64_bits(t_nm *nm, void *ptr)
 {
-	unsigned int	magic_number;
+	struct mach_header_64	*header;
+	struct nlist_64			*nlist;
+	struct load_command		*lc;
+	struct symtab_command	*sym;
+	char					*stringtable;
+	uint32_t				i;
+	uint32_t				ncmds;
+	uint32_t				j;
 
-	magic_number = *((int*)ptr);
-	if (magic_number == MH_MAGIC_64)
+	header = (struct mach_header_64*)ptr;
+	ft_printf("filetype: %p\n", header->filetype);
+	ncmds = header->ncmds;
+	lc = ptr + sizeof(*header);
+	i = 0;
+	while (i < ncmds)
 	{
-		ft_putendl("64");
-	}
-	else if (magic_number == MH_MAGIC)
-	{
-		ft_putendl("32");
+		if (lc->cmd == LC_SYMTAB)
+		{
+			sym = (struct symtab_command *)lc;
+			ft_printf("nb symboles: %d\n", sym->nsyms);
+			nlist = (void*)ptr + sym->symoff;
+			stringtable = (void*)ptr + sym->stroff;
+			j = 0;
+			while (j < sym->nsyms)
+			{
+				ft_putendl(stringtable + nlist[j].n_un.n_strx);
+				++j;
+			}
+			break ;
+		}
+		else if (lc->cmd == LC_SEGMENT)
+		{
+			ft_putendl("it's a segment !");
+		}
+		else if (lc->cmd == LC_DYSYMTAB)
+		{
+			ft_putendl("it's a dysymtab !");
+		}
+		lc = (void*)lc + lc->cmdsize;
+		++i;
 	}
 	(void)nm;
+
+}
+
+static int	name_list(t_nm *nm, void *ptr)
+{
+	uint32_t	magic_number;
+	int			swap;
+
+	magic_number = *((int*)ptr);
+	ft_printf("magic: %p\n", magic_number);
+	swap = (magic_number == MH_CIGAM_64 || magic_number == MH_CIGAM);
+	if (magic_number == MH_MAGIC_64 || magic_number == MH_CIGAM_64)
+	{
+		ft_putendl("64 bits");
+		handle_64_bits(nm, ptr);
+	}
+	else if (magic_number == MH_MAGIC || magic_number == MH_CIGAM)
+	{
+		ft_putendl("32 bits");
+	}
+	else
+	{
+		ft_putendl("unknown");
+	}
 	return (1);
 }
 
