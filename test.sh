0323ff80797064cf4a715a555ba7390d347775fc
Fanny Huang <>
2018-03-06 03:02:22 +0100

    Update test script

--- test.sh
+++ test.sh
@@ -2,17 +2,18 @@
 
 #############
 
-bin_mine=./ft_nm
-bin_theirs=nm
+bin_nm_mine=./ft_nm
+bin_nm=nm
+bin_otool_mine=./ft_otool
+bin_otool=otool
 
 ##############
 
 IFS=';'
-command_args=(\
-"ft_nm;"\
-"-r ft_nm;"\
-"-nr ft_nm;"\
-"-U ft_nm;"\
+
+dirs=(\
+"/bin;"\
+"/usr/bin;"\
 )
 
 #############
@@ -20,16 +21,31 @@ output_mine="mine"
 output_theirs="theirs"
 ##############
 
-for command_arg in $command_args; do
-	args=($command_arg)
-	IFS=" "
-	$bin_mine ${args[@]} > $output_mine
-	$bin_theirs ${args[@]} > $output_theirs
+function print_result() {
 	diff $output_mine $output_theirs
-
 	if [ $? -eq 0 ]; then
-		echo "\033[0;32m""√ \t$command_arg""\033[0;0m"
+		echo "\033[0;32m""√ \t $1""\033[0;0m"
 	else
-		echo "\033[0;31m""✗ \t$command_arg""\033[0;0m"
+		echo "\033[0;31m""✗ \t $1""\033[0;0m"
 	fi
+	rm -r $output_mine $output_theirs
+}
+
+function check_nm() {
+	$bin_nm_mine $1 > $output_mine
+	$bin_nm $1 > $output_theirs
+	print_result $1
+}
+
+function check_otool() {
+	$bin_otool_mine $1 > $output_mine
+	$bin_otool -t $1 > $output_theirs
+	print_result $1
+}
+
+for dir in $dirs; do
+	for file in $dir/*; do
+		check_nm $file
+		check_otool $file
+	done
 done
