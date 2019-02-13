d05251a167e7cc972e5dc9ff96b475d00e8dba62
Fanny HUANG <fhuang@e2r4p20.42.fr>
2019-02-13 17:35:24 +0100

    Fix -n --> sort when symbol's values are equal

--- src/nm/symbol/get_cmp_function.c
+++ src/nm/symbol/get_cmp_function.c
@@ -6,7 +6,7 @@
 /*   By: fhuang <fhuang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/18 18:13:55 by fhuang            #+#    #+#             */
-/*   Updated: 2019/01/28 19:44:20 by fhuang           ###   ########.fr       */
+/*   Updated: 2019/02/13 17:33:07 by fhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,27 @@ int		symbol_cmp_r_name(t_symbol sym1, t_symbol sym2)
 
 int		symbol_cmp_r_value(t_symbol sym1, t_symbol sym2)
 {
-	return (!(sym1.value > sym2.value));
+	if (sym1.value == sym2.value)
+	{
+		if ((sym1.type != sym2.type) && (sym1.type == 'U' || sym2.type == 'U'\
+			|| sym1.type == 'u' || sym2.type == 'u'))
+			return (sym1.type == 'U' ? 1 : 0);
+		else
+			return (symbol_cmp_r_name(sym1, sym2));
+	}
+	return (sym1.value < sym2.value);
 }
 
 int		symbol_cmp_value(t_symbol sym1, t_symbol sym2)
 {
+	if (sym1.value == sym2.value)
+	{
+		if ((sym1.type != sym2.type) && (sym1.type == 'U' || sym2.type == 'U'\
+			|| sym1.type == 'u' || sym2.type == 'u'))
+			return (sym1.type == 'U' ? 0 : 1);
+		else
+			return (symbol_cmp_name(sym1, sym2));
+	}
 	return (sym1.value > sym2.value);
 }
 
